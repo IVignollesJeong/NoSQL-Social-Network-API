8fce2a0c2866b4f2dbeb53a82094a0a15023895a
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-09-10 10:23:33 -0400

    add user model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/]
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    });
+
+    userSchema.virtual('friendCount').get(function () {
+        return this.friends.length;
+    });
+
+    const User = model('user', userSchema);
+
+    module.exports = User;
